5491b8eff2f30e9c757bda5681699d0ff3345ceb
Daitas <48018386+Daitas@users.noreply.github.com>
2019-03-26 09:47:19 -0300

    Versão estruturada

--- App10/App10/MainPage.xaml.cs
+++ App10/App10/MainPage.xaml.cs
@@ -13,5 +13,30 @@ namespace App10
 		{
 			InitializeComponent();
 		}
-	}
+
+
+        void OneButtonCLicket()
+        {
+            //desabilitar mensagens
+            Label1.IsVisible = false;
+            Label2.IsVisible = false;
+            //verificar se periodo e semestre foram selecionados
+            if(Entry2.Text != null &&
+               Entry2.Text.Lenght >0 &&
+               Picker.SelectedIndex >=0)
+            {
+                //criar variáveis
+                int periodo = int.Parse(Entry2.Text);
+                int semestre = Picker.SelectedIndex + 1;
+                //verificar se os valores são iguais
+                if (periodo==semestre)
+                {
+                    //habilitar mensagem de sucesso
+                    Label1.IsVisible = true;
+                }
+                else
+                {
+                    Label2.IsVisible = true;
+                }
+	    }
 }
